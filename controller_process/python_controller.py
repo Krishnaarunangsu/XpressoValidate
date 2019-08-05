c8a73796c025aa600d859529ee289f6faf674005
Krishnaarunangsu <arunangsutech@gmail.com>
2019-08-05 13:23:58 +0530

    Controller

--- controller_process/python_controller.py
+++ controller_process/python_controller.py
@@ -13,3 +13,17 @@ class ControllerExec:
         cmd = 'pwd'
         returned_output = subprocess.check_output(cmd)
         print('Output:', returned_output)
+
+print('Xpresso AI-Abzooba')
+print('***************************************')
+from subprocess import check_output
+
+
+
+#print(check_output("ls -l", shell=True).decode())
+#print(check_output("echo $PYTHONPATH", shell=True).decode())
+print('Docker Installation Check')
+print('***************************************')
+pwd='admin1'
+cmd='xprctl login -u admin1'
+print(check_output("echo {} | sudo -S {}".format(cmd, pwd), shell=True).decode())
